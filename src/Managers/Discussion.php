34c73c0ac66f3f57e0382f725e67c4a03f1b9610
Jasper Vriends <4417659+jaspervriends@users.noreply.github.com>
2021-03-19 23:33:52 +0100

    Refactoring discussion manager

--- src/Managers/Discussion.php
+++ src/Managers/Discussion.php
@@ -2,6 +2,7 @@
 namespace V17Development\FlarumSeo\Managers;
 
 use Flarum\Discussion\DiscussionRepository;
+use Flarum\User\UserRepository;
 use V17Development\FlarumSeo\Listeners\PageListener;
 
 /**
@@ -10,76 +11,66 @@ use V17Development\FlarumSeo\Listeners\PageListener;
  */
 class Discussion
 {
-    // Parent and Discussion Repository
+    /**
+     * @var PageListener
+     */
     protected $parent;
+
+    /**
+     * @var DiscussionRepository
+     */
     protected $discussionRepository;
 
+    /**
+     * @var UserRepository
+     */
+    protected $userRepository;
+
     // Current discussion
-    protected $discussionId = null;
-    protected $discussion = null;
     protected $firstPost = null;
+
+    /**
+     * @param DiscussionRepository $discussionRepository
+     */
+    public function __construct(
+        DiscussionRepository $discussionRepository, 
+        UserRepository $userRepository
+    ) {
+        $this->discussionRepository = $discussionRepository;
+        $this->userRepository = $userRepository;
+    }
 
     /**
      * Discussion constructor.
      * @param PageListener $parent
-     * @param DiscussionRepository $discussionRepository
      * @param $discussionId
      */
-    public function __construct(PageListener $parent, DiscussionRepository $discussionRepository, $discussionId)
+    public function handle(PageListener $parent, $discussionId)
     {
         $this->parent = $parent;
-        $this->discussionRepository = $discussionRepository;
-        $this->discussionId = $discussionId;
 
-        // Found discussion and discussion
-        $this->findDiscussion();
-
-        // Discussion not found
-        if($this->discussion === null) return;
-
-        // Create tags
-        $this->createTags();
-    }
-
-    /**
-     * Find discussion
-     *
-     * @return bool
-     */
-    private function findDiscussion()
-    {
         try {
             // Find discussion
-            $this->discussion = $this->discussionRepository->findOrFail($this->discussionId);
-
-            // Discussion not found
-            if($this->discussion === null)
-            {
-                return false;
-            }
+            $discussion = $this->discussionRepository->findOrFail($discussionId);
 
             // Find first post
-            $post = $this->discussion->firstPost()->get()->getDictionary();
+            $post = $discussion->firstPost()->get()->getDictionary();
 
-            $this->firstPost = array_shift($post);
+            $firstPost = array_shift($post);
 
-            // First post not found
-            if($this->firstPost === null) {
-                return false;
-            }
+            $this->createTags($discussion, $firstPost);
         }
         catch (\Exception $e) {
             // Do nothing. It just did not work
-            return false;
         }
-
-        return true;
     }
 
     /**
      * Create tags
+     * @param $discussion
+     * @param $firstPost
      */
-    private function createTags()
+    private function createTags($discussion, $firstPost)
     {
         // Update ld-json
         $this->parent
@@ -89,16 +80,16 @@ class Discussion
             ->setMetaPropertyTag('og:type', 'article');
 
         // Get last posted on
-        $lastPostedOn = $this->firstPost !== null ? $this->firstPost->getAttribute('edited_at') : $this->discussion->getAttribute('last_posted_at');
+        $lastPostedOn = $firstPost !== null ? $firstPost->getAttribute('edited_at') : $discussion->getAttribute('last_posted_at');
 
         // Set short description
         $this->parent
-            ->setTitle($this->discussion->getAttribute('title'), true)
-            ->setPublishedOn($this->discussion->getAttribute('created_at'));
+            ->setTitle($discussion->getAttribute('title'), true)
+            ->setPublishedOn($discussion->getAttribute('created_at'));
 
         // Set discussion description, only when a first post exists
-        if($this->firstPost !== null) {
-            $content = $this->firstPost->formatContent();
+        if($firstPost !== null) {
+            $content = $firstPost->formatContent();
 
             // Set page description
             $this->parent
@@ -115,11 +106,11 @@ class Discussion
         }
 
         // Update topic url
-        $this->parent->setUrl('/d/' . $this->discussion->getAttribute('id') . '-' . $this->discussion->getAttribute('slug'));
+        $this->parent->setUrl('/d/' . $discussion->getAttribute('id') . '-' . $discussion->getAttribute('slug'));
 
         try {
             // Add author to the page meta data
-            $findUser = $this->parent->getUser($this->discussion->getAttribute('user_id'));
+            $findUser = $this->userRepository->findOrFail($discussion->getAttribute('user_id'));
 
             // Set author data if found
             if ($findUser !== null) {
