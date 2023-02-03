bf52d762db27686684c196a4647eadee6d30aaa6
Jasper Vriends <jasper@vriends.co>
2023-02-03 17:32:19 +0100

    Query SEO Meta by Model object or object type

--- src/SeoMeta/SeoMeta.php
+++ src/SeoMeta/SeoMeta.php
@@ -4,17 +4,20 @@ namespace V17Development\FlarumSeo\SeoMeta;
 
 use Carbon\Carbon;
 use Flarum\Database\AbstractModel;
+use Illuminate\Database\Eloquent\Model;
 
 class SeoMeta extends AbstractModel
 {
     protected $table = 'seo_meta';
+
+    protected $fillable = ['object_id', 'object_type'];
 
     /**
      * {@inheritdoc}
      */
     protected $dates = ['created_at', 'updated_at'];
 
-    public static function build(int $objectId, string $objectType, bool $autoUpdate = true)
+    public static function build(string $objectType, int $objectId, bool $autoUpdate = true)
     {
         $seoMeta = new static();
         $seoMeta->object_id = $objectId;
@@ -23,5 +26,45 @@ class SeoMeta extends AbstractModel
         $seoMeta->created_at = Carbon::now();
 
         return $seoMeta;
+    }
+
+    /**
+     * Find the SEO meta by object type
+     * 
+     * @param string $objectType Name of the object
+     * @param string $objectId ID of the object
+     */
+    public static  function findByObjectType(string $objectType, int $objectId): Model
+    {
+        return self::firstOrCreate([
+            'object_type' => $objectType,
+            'object_id' => $objectId
+        ]);
+    }
+
+    /**
+     * Find the SEO meta of an object from a model
+     * 
+     * @param Model $model The model
+     */
+    public static  function findOneByModel(Model $model): ?Model
+    {
+        return self::where([
+            'object_type' => $model->getTable(),
+            'object_id' => $model->getKey()
+        ])->firstOrNull();
+    }
+
+    /**
+     * Find or create the SEO meta of an object from a model
+     * 
+     * @param Model $model The model
+     */
+    public static  function findByModelOrCreate(Model $model): Model
+    {
+        return self::firstOrCreate([
+            'object_type' => $model->getTable(),
+            'object_id' => $model->getKey()
+        ]);
     }
 }
